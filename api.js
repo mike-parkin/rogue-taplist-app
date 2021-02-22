4faf221649ceab7b3798e3ee6c23348b451db7df
student <student@eda-bc-11.localdomain>
2021-02-23 11:51:17 +1300

    data converting from xml to js

--- api.js
+++ api.js
@@ -1,16 +1,32 @@
 import request from "superagent";
-// import xml2jsParser from 'superagent-xml2jsparser'
+import xml2js from 'react-native-xml2js'
 
-const Url =
-  "http://www.craftbeercapital.com/api/beta/?key=TY9ERWB92X&output=xml&request=purveyor&id=31&list=tap";
+
+const Url = "http://www.craftbeercapital.com/api/beta/?key=TY9ERWB92X&output=xml&request=purveyor&id=31&list=tap";
 
 export function getTaplist() {
-  return (
-    request
+   return request
       .get(Url)
       .accept("xml")
       .buffer(true)
       // .parse(xml2jsParser)
-      .then((res) => res.text)
-  );
+      .then((res) => {
+        // const parsedData = xml2js.parseString(res.text, (err, result) => {
+          return new Promise((resolve, reject) => {
+            xml2js.parseString(res.text, (err, result) => {
+              if(err) reject(err)
+              else {
+                console.log(result)
+                const parsedData = {
+                  name: result.BeerList.Purveyor[0].Name[0],
+                  beerList: result.BeerList.Beers[0].Beer
+                }
+                resolve(parsedData)
+              }
+          })
+          
+          
+        })   
+        // console.log(parsedData)
+      })
 }
--- components/Main.js
+++ components/Main.js
@@ -3,22 +3,40 @@ import { getTaplist } from '../api'
 import { 
   View, 
   StyleSheet,
-  Text
+  Text,
+  FlatList
   } from 'react-native'
+  import { List, ListItem } from 'react-native-elements'
 
 const Main = () => {
+  const [list, setList] = useState({})
 
   useEffect(() => {
     getTaplist()
-    .then(list => {
-      console.log(list)
+    .then(result => {
+      console.log(result.beerList)
+      setList(result)
     })
   }, [])
-
+  // const purveyor = list.Purveyor
+  // const beers = list.Beers
+  // const BeerList = beers[0].Beer
 
   return(
     <View>
-      <Text>Hello</Text>
+      <Text>{list.name}</Text>
+      <List>
+        <FlatList 
+          data={list.beerlist}
+          renderItem={({ item }) => (
+            <ListItem>
+              <Text>brewery: {item.brewery[0]}</Text>
+              <Text>beerName: {item.name[0]}</Text>
+              <Text>beerStyle: {item.style[0]}</Text>
+            </ListItem>
+          )}
+        />
+      </List>     
     </View>
   )
 }
